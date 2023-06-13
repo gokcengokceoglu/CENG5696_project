ba36d7ce36d66f75b727e77d3f4aa9627122ed7f
Gokcen Gokceoglu <33410530+gokcengokceoglu@users.noreply.github.com>
2023-06-13 13:48:01 +0300

    Update vectorize_lyrics-word2vec.py

--- vectorize_lyrics-word2vec.py
+++ vectorize_lyrics-word2vec.py
@@ -20,85 +20,163 @@ import numpy as np
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
+import tqdm
 ps = PorterStemmer()
 
 nltk.download('stopwords')
 print(stopwords.words('english'))
 stop_words = set(stopwords.words('english'))
 
-f = open('queries.json')
-  
-# returns JSON object as 
-# a dictionary
-data = json.load(f)
 
-words = []
-for elm in data:
-    query = elm['query']
+from gensim.models.callbacks import CallbackAny2Vec
+
+class callback(CallbackAny2Vec):
+    '''Callback to print loss after each epoch.'''
+
+    def __init__(self):
+        self.epoch = 0
+
+    def on_epoch_end(self, model):
+        loss = model.get_latest_training_loss()
+        print('Loss after epoch {}: {}'.format(self.epoch, loss))
+        self.epoch += 1
+
+def get_dataset(query_file, lyrics_file):
+    f_query = open(query_file)
+    query_data = json.load(f_query)
+    f_lyric = open(lyrics_file)
+    lyric_data = json.load(f_lyric)
+    sentences = []
+    for elm in query_data:
+        query = elm['query']
+        word_tokens = word_tokenize(query)
+        filtered_sentence = [w for w in word_tokens if not w.lower() in stop_words]
+        filtered_sentence = [ps.stem(w) for w in filtered_sentence]
+        sentences.append(filtered_sentence)
+    for elm in lyric_data:
+        lyric = elm['lyrics']
+        word_tokens = word_tokenize(lyric)
+        filtered_sentence = [w for w in word_tokens if not w.lower() in stop_words]
+        filtered_sentence = [ps.stem(w) for w in filtered_sentence]
+        sentences.append(filtered_sentence)
+    return sentences
+
+
+
+
+def train_word2vec(words, model_type="skipgram", vector_size = 100, window = 5, epochs = 10):
+
+    if (model_type == "skipgram"):
+         model = gensim.models.Word2Vec(words, min_count = 1, vector_size = 100,
+                                            window = 5, sg = 1, callbacks=[callback()])
+    else: # Train C-BOW Model
+        model = gensim.models.Word2Vec(words, min_count = 1,
+                                        vector_size = 100, window = 5, epochs=epochs, callbacks=[callback()])
+    return model
+    
+  
+def visualize_wordmap(model):
+    X = []
+    ii = 0
+    words_vis = []
+    for word in model.wv.index_to_key:
+        ii+=1
+        print(word)
+        if (len(word) < 5):
+            continue
+        #X.append(word)
+        X.append(model.wv.get_vector(word))
+        words_vis.append(word)
+        if (ii > 200):
+            break
+
+    #X = model1[model1.wv.index_to_key]
+    pca = PCA(n_components=2)
+
+    result = pca.fit_transform(X)
+
+    # create a scatter plot of the projection
+    plt.scatter(result[:, 0], result[:, 1], s=3)
+    words = list(model.wv.index_to_key)
+
+    for i, word in enumerate(words_vis):
+        plt.annotate(word, xy=(result[i, 0], result[i, 1]), fontsize=8)
+
+    plt.show()
+
+def get_song_vectors(model, lyrics_file, vector_size = 100):
+    song_word2vec_embeddings = []
+    f_lyric = open(lyrics_file)
+    lyric_data = json.load(f_lyric)
+    for elm in tqdm.tqdm(lyric_data):
+        num_words = 0
+        song_vector = np.zeros((vector_size,))
+        lyric = elm['lyrics']
+        title = elm['title']
+        word_tokens = word_tokenize(lyric)
+        filtered_sentence = [w for w in word_tokens if not w.lower() in stop_words]
+        filtered_sentence = [ps.stem(w) for w in filtered_sentence]
+        for word in filtered_sentence:
+            vect = model.wv.get_vector(word)
+            song_vector += vect
+            num_words += 1
+        mean_vector = song_vector / num_words
+        song_word2vec_embeddings.append([title, list(mean_vector)])
+    with open("song_word2vec_embeddings.json", "w") as fp:
+        json.dump(song_word2vec_embeddings, fp)
+    return
+
+def calc_cosine_sim(A,B):
+    cosine = np.dot(A,B)/(np.linalg.norm(A)*np.linalg.norm(B))
+    return cosine
+
+def sort_sims(list):
+    list.sort(key = lambda x: x[1])
+    return list
+
+def rank_songs(model, query, vector_size=100):
+    num_words = 0
+    query_vector = np.zeros((vector_size,))
     word_tokens = word_tokenize(query)
-    # converts the words in word_tokens to lower case and then checks whether
-    #they are present in stop_words or not
     filtered_sentence = [w for w in word_tokens if not w.lower() in stop_words]
     filtered_sentence = [ps.stem(w) for w in filtered_sentence]
-    #with no lower case conversion
-    words.append(filtered_sentence)
-    print(filtered_sentence)
-        # song = elm['song'].split(" ")
-        #     words.append(s)
+    for word in filtered_sentence:
+        vect = model.wv.get_vector(word)
+        query_vector += vect
+        num_words += 1
+    mean_query_vector = query_vector / num_words
+    song_w2v = open("song_word2vec_embeddings.json")
+    song_embeddings = json.load(song_w2v)
+    sims = []
+    for elm in song_embeddings:
+        title = elm[0]
+        vect = np.asarray(elm[-1])
+        sim = calc_cosine_sim(vect, mean_query_vector)
+        sims.append([title,sim])
+    ranked_songs = sort_sims(sims)
+    return ranked_songs
+
+def test(query_file,model):
+    file1 = open("myfile.txt","w+")
+    f_query = open(query_file)
+    query_data = json.load(f_query)
+    for elm in query_data:
+        query = elm['query']
+        song = elm['song']
+        ranked_songs = rank_songs(model, query, vector_size=100)
+        first_songs = ranked_songs[-5:-1]
+        file1.write(str(song) + "\n")
+        for f in first_songs:
+            file1.write(str(f) + "\n")
+            file1.write("-------------------\n")
+    file1.close()
 
 
-# Create CBOW model
-# model1 = gensim.models.Word2Vec(words, min_count = 1,
-# 							vector_size = 100, window = 5, epochs=50)
+query_file = "queries.json"
+lyrics_file = "lyrics.json"
 
-model1 = gensim.models.Word2Vec(words, min_count = 1, vector_size = 100,
-										window = 5, sg = 1)
-X = []
-ii = 0
-words_vis = []
-for word in model1.wv.index_to_key:
-    ii+=1
-    print(word)
-    if (word == "shot"):
-        continue
-    #X.append(word)
-    X.append(model1.wv.get_vector(word))
-    words_vis.append(word)
-    if (ii > 100):
-        break
-
-
-#X = model1[model1.wv.index_to_key]
-pca = PCA(n_components=2)
-
-result = pca.fit_transform(X)
-
-# create a scatter plot of the projection
-plt.scatter(result[:, 0], result[:, 1], s=3)
-words = list(model1.wv.index_to_key)
-
-for i, word in enumerate(words_vis):
-   plt.annotate(word, xy=(result[i, 0], result[i, 1]), fontsize=8)
-
-plt.show()
-# # Print results
-# print("Cosine similarity between 'alice' " +
-# 			"and 'wonderland' - CBOW : ",
-# 	model1.wv.similarity('stop', 'just'))
-	
-# # print("Cosine similarity between 'alice' " +
-# # 				"and 'machines' - CBOW : ",
-# # 	model1.wv.similarity('alice', 'machines'))
-
-# # Create Skip Gram model
-# model2 = gensim.models.Word2Vec(data, min_count = 1, vector_size = 100,
-# 											window = 5, sg = 1)
-
-# # Print results
-# print("Cosine similarity between 'alice' " +
-# 		"and 'wonderland' - Skip Gram : ",
-# 	model2.wv.similarity('stop', 'just'))
-	
-# # print("Cosine similarity between 'alice' " +
-# # 			"and 'machines' - Skip Gram : ",
-# # 	model2.wv.similarity('alice', 'machines'))
+ds = get_dataset(query_file, lyrics_file)
+model = train_word2vec(ds, model_type="skipgram", vector_size = 100, window = 5, epochs = 10)
+get_song_vectors(model, lyrics_file, vector_size = 100)
+test(query_file, model)
+#visualize_wordmap(model)
